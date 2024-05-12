f94a7e9438db9f4e339ab66029ee5e395fcbc253
Christian Grimberg <christian.grimberg@outlook.com>
2024-05-12 19:43:11 +0000

    :computer: Se crean los esqueletos y se instancias las clases faltandes.
    Signed-off-by: Christian Grimberg <christian.grimberg@outlook.com>

--- app/Program.cs
+++ app/Program.cs
@@ -4,7 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Usuario usuarioDePrueba = new();
-        Console.WriteLine(usuarioDePrueba);
+        Usuario usuario1 = new();
+        Producto producto1 = new();
+        ProductoVendido productoVendido1 = new();
+        Venta venta1 = new();
+
+        Console.WriteLine("{0}{1}{2}{3}",
+            usuario1, producto1, productoVendido1, venta1);
     }
 }
