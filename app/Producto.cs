91cf540be4b0d683df28fa3e28f4862b6e343bbc
Christian Grimberg <christian.grimberg@outlook.com>
2024-05-12 20:08:05 +0000

    :computer: Se implementa la propiedad Id y el metodo ToString().
    Signed-off-by: Christian Grimberg <christian.grimberg@outlook.com>

--- app/Producto.cs
+++ app/Producto.cs
@@ -1,6 +1,32 @@
-﻿namespace app;
+﻿using System.Reflection;
+
+namespace app;
 
 public class Producto
 {
+	private int id;
+	public int Id
+	{
+		get
+		{
+			if(id.GetType() != typeof(int)) id = 0;
+			return id;
+		}
+		set
+		{
+			if(value.GetType() == typeof(int)) id = value;
+			else id = 0;
+		}
+	}
 
+	public override string ToString()
+	{
+		string returnedValue = string.Empty;
+
+		foreach(PropertyInfo propertyInfo in this.GetType().GetProperties())
+		{
+			returnedValue += string.Format("El {0} de Producto es: {1}\n", propertyInfo.Name, propertyInfo.GetValue(this));
+		}
+		return returnedValue;
+	}
 }
