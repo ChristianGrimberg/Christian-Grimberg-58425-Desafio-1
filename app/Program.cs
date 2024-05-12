4923e9f5bf085e372c91d5700532f3a632f568e3
Christian Grimberg <christian.grimberg@outlook.com>
2024-05-12 21:37:10 +0000

    :computer: Se usan ejemplos instanciados con valores en las propiedades en el programa de consola.
    Signed-off-by: Christian Grimberg <christian.grimberg@outlook.com>

--- app/Program.cs
+++ app/Program.cs
@@ -4,10 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Usuario usuario1 = new();
-        Producto producto1 = new();
-        ProductoVendido productoVendido1 = new();
-        Venta venta1 = new();
+        Usuario usuario1 = new(1,"Juan","Perez","juan.perez","1234","juan.perez@mail.com");
+        Producto producto1 = new(1, "Computadora", 1000000.00f, 1500000.00f, 10, 1);
+        ProductoVendido productoVendido1 = new(1, 1, 2, 1);
+        Venta venta1 = new(1, "2 x Computadora", 1);
+
+        Console.WriteLine(producto1.Costo);
 
         Console.WriteLine("{0}{1}{2}{3}",
             usuario1, producto1, productoVendido1, venta1);
